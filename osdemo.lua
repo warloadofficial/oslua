01b679225d26f54b57d509c308dd450db5896dd0
WarLoad Official <38010043+warloadofficial@users.noreply.github.com>
2018-07-10 15:17:11 +0300

    Add files via upload

---
+++ osdemo.lua
@@ -0,0 +1,52 @@
+local function as()
+  print("WarLoad OS Lua Edition")
+  io.write("Введите ваше имя")
+  name = io.read()
+  print("Ваше имя: "..name)
+end
+
+as()
+
+local function fgd()
+  print("Loading")
+end
+
+fgd()
+
+for i = 1, 10 do
+  print(i)
+end
+
+local function fgh()
+  print("Copyright 2018 WarLoad Map Company")
+  print("Наша операционная система написана на очень простом скриптовом языке программирования Lua")
+  io.write("Введите ваш пароль")
+  password = io.read()
+  print("Ваш пароль: "..password)
+end
+
+fgh()
+
+local function tgm()
+  print("Сейчас программа произведет необходимые математические расчеты")
+end
+
+tgm()
+
+function f(x,y)
+  return x*y
+end
+
+print(f(5,3))
+
+function h(d,c)
+  return d+c
+end
+
+print(h(6,5))
+
+local function jh()
+  print("Все необходимые математические расчеты выполнены")
+end
+
+jh()
